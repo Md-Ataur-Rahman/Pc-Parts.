2b384997f140eaa118ef62cd3ecfe1a076665f8e
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-22 15:58:52 +0600

    navbar added

---
+++ src/Component/Navbar/Navbar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+const Navbar = () => {
+  const navLinks = (
+    <>
+      <li>
+        <NavLink to="/">Home</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <div class="navbar bg-base-100 w-5/6 mx-auto">
+      <div class="navbar-start">
+        <div class="dropdown">
+          <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabindex="0"
+            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLinks}
+          </ul>
+        </div>
+        <a class="btn btn-ghost normal-case text-xl">Pc Parts.</a>
+      </div>
+      <div class="navbar-end hidden lg:flex">
+        <ul class="menu menu-horizontal p-0">{navLinks}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
