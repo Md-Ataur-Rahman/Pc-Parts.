b1d37f0f920668673a9c84bdce46ba2b1626926d
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-26 09:30:19 +0600

    not found setup done

--- src/Component/NotFound/NotFound.js
+++ src/Component/NotFound/NotFound.js
@@ -5,15 +5,24 @@ import NotFoundImage from "../../Images/errorpage.png";
 const NotFound = () => {
   return (
     <>
-      <img src={NotFoundImage} alt="" />
-      <h3>We Are Lost ...</h3>
-      <p>
-        THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN MOVED, RENAMED, OR MIGHT
-        NEVER EXISTED.
-      </p>
-      <Link to="/">
-        <button className="primary_btn">Send Back</button>
-      </Link>
+      <div class="hero min-h-screen bg-base-200">
+        <div class="hero-content flex-col lg:flex-row">
+          <img
+            src={NotFoundImage}
+            class="md:w-3/5 max-w-sm rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 class="text-5xl font-bold">We Are Lost ...</h1>
+            <p class="py-6">
+              THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN MOVED, RENAMED, OR
+              MIGHT NEVER EXISTED.
+            </p>
+            <Link to="/">
+              <button class="btn btn-outline btn-primary">Send Back</button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
