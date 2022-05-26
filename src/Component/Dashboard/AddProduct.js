75f624ba8ae821bac4ee658e10bdd3b4d1b4bcaa
Md-Ataur-Rahman <mdataurrahmanm505@gmail.com>
2022-05-26 07:45:17 +0600

    add product section

--- src/Component/Dashboard/AddProduct.js
+++ src/Component/Dashboard/AddProduct.js
@@ -1,7 +1,223 @@
 import React from "react";
+import { useQuery } from "react-query";
+import { toast } from "react-toastify";
+import { useForm } from "react-hook-form";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
-};
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm();
 
+  const { data: products, isLoading } = useQuery("services", () =>
+    fetch("http://localhost:5000/services").then((res) => res.json())
+  );
+
+  const imageStorageKey = "c7ad20aec42c81da7add47a072308b4a";
+
+  const onSubmit = async (data) => {
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+    console.log(formData);
+    const url = `https://api.imgbb.com/1/upload?key=${imageStorageKey}`;
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.success) {
+          const img = result.data.url;
+          console.log(img);
+          const product = {
+            name: data.productName,
+            image: img,
+            descriptions: data.description,
+            orderQuantity: data.orderQuantity,
+            quantity: data.quantity,
+            perPrice: data.perPrice,
+          };
+          console.log(product);
+          fetch("http://localhost:5000/product", {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+              authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            },
+            body: JSON.stringify(product),
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              if (data.insertedId) {
+                toast.success("Product added successfully");
+                reset();
+              } else {
+                toast.error("Failed to add the Product");
+              }
+            });
+        }
+      });
+  };
+
+  if (isLoading) {
+    return <p>Loading</p>;
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center md:w-9/12">
+      <h2 className="text-2xl">Add a Product</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Product Name"
+            className="input input-bordered w-full max-w-xs"
+            {...register("productName", {
+              required: {
+                value: true,
+                message: "Product Name is Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.text?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.text.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Description</span>
+          </label>
+          <input
+            type="text"
+            placeholder="Description"
+            className="input input-bordered w-full max-w-xs"
+            {...register("description", {
+              required: {
+                value: true,
+                message: "Description is Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.text?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.Description.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Order Quantity</span>
+          </label>
+          <input
+            type="number"
+            placeholder="Order Quantity"
+            className="input input-bordered w-full max-w-xs"
+            {...register("orderQuantity", {
+              required: {
+                value: true,
+                message: "Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.number?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.number.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Available Quantity</span>
+          </label>
+          <input
+            type="number"
+            placeholder="Available Quantity"
+            className="input input-bordered w-full max-w-xs"
+            {...register("quantity", {
+              required: {
+                value: true,
+                message: "Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.number?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.number.message}
+              </span>
+            )}
+          </label>
+        </div>
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Per Qauntity</span>
+          </label>
+          <input
+            type="number"
+            placeholder="Per Price"
+            className="input input-bordered w-full max-w-xs"
+            {...register("perPrice", {
+              required: {
+                value: true,
+                message: "Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.number?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.number.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <div className="form-control w-full max-w-xs">
+          <label className="label">
+            <span className="label-text">Photo</span>
+          </label>
+          <input
+            type="file"
+            className="input input-bordered w-full max-w-xs"
+            {...register("image", {
+              required: {
+                value: true,
+                message: "Image is Required",
+              },
+            })}
+          />
+          <label className="label">
+            {errors.name?.type === "required" && (
+              <span className="label-text-alt text-red-500">
+                {errors.name.message}
+              </span>
+            )}
+          </label>
+        </div>
+
+        <input
+          className="btn w-full max-w-xs text-white"
+          type="submit"
+          value="Add"
+        />
+      </form>
+    </div>
+  );
+};
 export default AddProduct;
